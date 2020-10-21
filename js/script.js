4cd85c9f0d3444f057bb25a527d3fbcb85d08e63
ansaryergesh <yergeshovansar@gmail.com>
2020-10-22 05:43:04 +0600

    fix

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 
+   var modal = document.querySelector(".modalblock");
+        var btn = document.getElementById("icon1");
+
+        btn.addEventListener("click", () => {
+            modal.style.display = "block";
+            overlay.classList.add("active");
+        });
+
+        window.addEventListener("click", (e) => {
+            if (e.target == modal) {
+                modal.style.display = "none";
+            }
+        });
