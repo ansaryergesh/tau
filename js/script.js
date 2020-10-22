81cced02511cdc3f549b316a2b7f1502f326b256
ansaryergesh <yergeshovansar@gmail.com>
2020-10-22 10:41:18 +0600

    fix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 
-   var modal = document.querySelector(".modalblock");
-        var btn = document.getElementById("icon1");
+var modal = document.querySelector(".modalblock");
+var btn = document.getElementById("icon1");
+var closebtn = document.querySelector('.closebtn')
+btn.addEventListener("click", () => {
+    modal.style.display = "block";
+    overlay.classList.add("active");
+});
 
-        btn.addEventListener("click", () => {
-            modal.style.display = "block";
-            overlay.classList.add("active");
-        });
+window.addEventListener("click", (e) => {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+});
 
-        window.addEventListener("click", (e) => {
-            if (e.target == modal) {
-                modal.style.display = "none";
-            }
-        });
+closebtn.addEventListener("click", () => {
+        modal.style.display = "none";
+});
