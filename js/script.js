fd2c4f99188383cd2c88501abe479ac6159f4a45
ansaryergesh <yergeshovansar@gmail.com>
2020-10-23 12:22:12 +0600

    slider comp

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
 
-var modal = document.querySelector(".modalblock");
-var btn = document.getElementById("icon1");
+// var modal = document.querySelector(".modalblock");
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+
+var btn = document.querySelector(".icon");
 var closebtn = document.querySelector('.closebtn')
 btn.addEventListener("click", () => {
-    modal.style.display = "block";
-    overlay.classList.add("active");
+    var modal = btn.dataset.modalType;
+    document.style.display = "block";
 });
 
 window.addEventListener("click", (e) => {
-    if (e.target == modal) {
+    if (e.target != modal && modal.style.display == 'none') {
         modal.style.display = "none";
     }
 });
@@ -16,3 +41,5 @@ window.addEventListener("click", (e) => {
 closebtn.addEventListener("click", () => {
         modal.style.display = "none";
 });
+
+
