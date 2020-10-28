ffa4450c50148d5d2ba8c7e3ab7da5cf9813d048
ansaryergesh <yergeshovansar@gmail.com>
2020-10-28 10:04:39 +0600

    fix

--- js/script.js
+++ js/script.js
@@ -42,3 +42,40 @@ for(let i=0; i<modal.length; i++) {
     
 }
 
+
+// slider bar
+var indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n){
+    muestraSlides( indice+=n );
+}
+
+function posicionSlide(n){
+    muestraSlides(indice=n);
+}
+setInterval(function tiempo(){
+    muestraSlides(indice+=1)
+},6000);
+function muestraSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('miSlider');
+    var barras = document.getElementsByClassName('barra');
+
+    if(n > slides.length){
+        indice = 1;
+    }
+    if(n < 1){
+        indice = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < barras.length; i++){
+        barras[i].className = barras[i].className.replace(" active", "");
+    }
+
+    slides[indice-1].style.display = 'block';
+    barras[indice-1].className += ' active';
+
+}
